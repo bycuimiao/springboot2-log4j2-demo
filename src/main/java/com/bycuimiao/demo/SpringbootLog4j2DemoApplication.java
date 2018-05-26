72ca7afe700e267661849b936e6a4e7588873ad7
bycuimiao <373013128@qq.com>
2018-05-26 09:58:34 +0800

    commit

---
+++ src/main/java/com/bycuimiao/demo/SpringbootLog4j2DemoApplication.java
@@ -0,0 +1,12 @@
+package com.bycuimiao.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootLog4j2DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootLog4j2DemoApplication.class, args);
+	}
+}
---
+++ src/main/java/com/bycuimiao/demo/com/bycuimiao/demo/log/Application.java
@@ -0,0 +1,26 @@
+/*
+ * Created by cuimiao on 2018/5/26.
+ */
+
+package com.bycuimiao.demo.com.bycuimiao.demo.log;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @author cuimiao
+ * @version 0.0.1
+ * @Description:
+ * @since 0.0.1 2018-05-26
+ */
+public class Application {
+  Logger logger = LoggerFactory.getLogger(Application.class);
+  public void logApp(){
+    logger.info("log4j2 success");
+  }
+
+  public static void main(String[] args) {
+    Application application = new Application();
+    application.logApp();
+  }
+}
